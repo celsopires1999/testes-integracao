5ad59047d8b8df272ec9dcef124ecc62b18af6c2
celsopires1999 <celso.pires1999@gmail.com>
2021-09-10 11:06:17 -0300

    add .env.testing

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2021-09-05 00:08:31.
+    * Generated on 2021-09-10 13:29:08.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
